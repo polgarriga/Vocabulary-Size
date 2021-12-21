6fa9df706ef92c6444d6467b89f31eae50af7cc4
polgr98 <50044795+polgr98@users.noreply.github.com>
2021-12-21 11:07:20 +0100

    Update options.py

--- options.py
+++ options.py
@@ -1,8 +1,15 @@
-# -*- coding: utf-8 -*-
-
 import streamlit as st
+import pandas as pd
 
-st.write("""
-# Heading
-This is my **first** *app*!
-""")
+@st.cache(allow_output_mutation=True)
+def get_data():
+    return []
+
+user_id = st.text_input("User ID")
+foo = st.slider("foo", 0, 100)
+bar = st.slider("bar", 0, 100)
+
+if st.button("Add row"):
+    get_data().append({"UserID": user_id, "foo": foo, "bar": bar})
+
+st.write(pd.DataFrame(get_data()))

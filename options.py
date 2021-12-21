9132bfb4b6d8a9cb9b5d01979190926ad8db6a2a
polgr98 <50044795+polgr98@users.noreply.github.com>
2021-12-21 12:07:19 +0100

    Update options.py

--- options.py
+++ options.py
@@ -4,21 +4,17 @@ import csv
 
 f = open('C:/Users/Pol/Documents/Database.csv', 'w')
 
-@st.cache(allow_output_mutation=True)
-def get_data():
-    return []
-
 user_id = st.text_input("User ID")
 foo = st.slider("foo", 0, 100)
 bar = st.slider("bar", 0, 100)
 
 writer = csv.writer(f)
 
-writer.writerow(df)
-
 if st.button("Add row"):
     
     df = [{"UserID": user_id, "foo": foo, "bar": bar}]
     df = pd.DataFrame(df)
+
+writer.writerow(df)
 
 f.close()
